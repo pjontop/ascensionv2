144d7ee0deeb9b2dae9d5bc96d01155c5fd182c1
pj <pramsu.pandey@outlook.com>
2026-03-27 18:11:38 -0400

    feat: add roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
+  enum :role, { user: 0, reviewer: 1, admin: 2, superadmin: 3 }
+
   def posthog_distinct_id
     email.presence || "user-#{id}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_20_193000) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_27_221059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_20_193000) do
     t.string "email"
     t.boolean "email_verified"
     t.string "name"
+    t.integer "role", default: 0, null: false
     t.string "slack_id"
     t.string "uid"
     t.datetime "updated_at", null: false
